885df8a5ff2a7f8b6bc77a0f9ab445c7cb1f33d3
Nishant Joshi <nishu.iitadv@gmail.com>
2025-08-30 21:08:50 +0530

    observer design pattern

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,91 @@
 import java.util.*;
 
-class MeetingRoom{
-    int id;
-    int capacity;
-    Calendar cal;
+// Observer interface
+interface IUser {
+    void update(String channelName, String newVideo);
 }
 
-class MeetingRoomManager{
-    List<MeetingRoom> meetRooms = new ArrayList<>(10);
+// Subject interface
+interface IChannel {
+    void subscribe(IUser user);
+    void unsubscribe(IUser user);
+    void notifyObservers(String newVideo);
+    void uploadVideo(String title);
 }
 
-class Calendar{
-    int start;
-    int end;
-}
+// Concrete Subject
+class Channel implements IChannel {
+    private String name;
+    private List<IUser> subscribers = new ArrayList<>();
 
-class User{
-    int id;
-    String name;
-    String email;
-}
+    Channel(String name) {
+        this.name = name;
+        System.out.println("New channel created: " + name);
+    }
 
-class MeetingScheduler{
-    MeetingRoom Scheduler(int people, int start, int end)
-    {
-        for(MeetingRoom mr : meetRooms)
-        {
-            if(mr.capacity>people && end<mr.cal.start) return mr;
+    @Override
+    public void subscribe(IUser user) {
+        subscribers.add(user);
+        System.out.println(user + " subscribed to " + name);
+    }
+
+    @Override
+    public void unsubscribe(IUser user) {
+        subscribers.remove(user);
+        System.out.println(user + " unsubscribed from " + name);
+    }
+
+    @Override
+    public void notifyObservers(String newVideo) {
+        for (IUser user : subscribers) {
+            user.update(name, newVideo);
         }
     }
 
+    @Override
+    public void uploadVideo(String title) {
+        System.out.println(name + " uploaded new video: " + title);
+        notifyObservers(title);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
 
+// Concrete Observer
+class User implements IUser {
+    private String username;
+
+    User(String username) {
+        this.username = username;
+        System.out.println("New user created: " + username);
+    }
+
+    @Override
+    public void update(String channelName, String newVideo) {
+        System.out.println(username + " got notified: " + channelName + " uploaded " + newVideo);
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
+
+// Driver class
 public class Main {
     public static void main(String[] args) {
-        MeetingScheduler ms = new MeetingScheduler();
-        MeetingRoom availableRoom = ms.Scheduler(11,9,12);
-        System.out.println("First available room "+availableRoom);
+        Channel channel = new Channel("CarryMinati");
+        User user1 = new User("Nishant");
+        User user2 = new User("Aditya");
+
+        channel.subscribe(user1);
+        channel.subscribe(user2);
+
+        channel.uploadVideo("Mast Roast");
+        channel.unsubscribe(user1);
+        channel.uploadVideo("Another Video");
     }
 }
